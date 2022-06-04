bfc2edf118de70af8fc8196e1d874e3c2b22a29b
Oscar Jacobsen <67653883+oscarjacobsen@users.noreply.github.com>
2022-06-04 22:04:12 +0200

    123

---
+++ ifcjs/ifc.js
@@ -0,0 +1,11 @@
+﻿function $(selector){
+
+    const self = {
+        element: document.querySelector(selector),
+        html: ()=> self.element,
+    }
+    return self
+}
+
+
+console.log($('h1').html())
