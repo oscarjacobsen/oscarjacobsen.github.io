3f4ec4bb6a1395604b47914c0861d59ff1a24ad1
Oscar Jacobsen <oscar-jacobsen@hotmail.com>
2022-08-04 12:35:25 +0200

    123

---
+++ SystemSpaceTime/1_install_libraries.bat
@@ -0,0 +1,6 @@
+@echo off
+call npm init .
+call npm install --legacy-peer-deps web-ifc-three
+call npm install --legacy-peer-deps three
+call npm install --legacy-peer-deps rollup --save-dev
+call npm install --legacy-peer-deps @rollup/plugin-node-resolve --save-dev
