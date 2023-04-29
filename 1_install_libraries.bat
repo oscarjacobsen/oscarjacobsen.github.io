3c6f95c321ad3a14527b7f502e80cc8534af6557
Oscar Jacobsen <Oscar.Jacobsen@norconsult.com>
2023-04-29 11:29:39 +0200

    123

--- 1_install_libraries.bat
+++ 1_install_libraries.bat
@@ -4,3 +4,4 @@ call npm install --legacy-peer-deps web-ifc-three
 call npm install --legacy-peer-deps three
 call npm install --legacy-peer-deps rollup --save-dev
 call npm install --legacy-peer-deps @rollup/plugin-node-resolve --save-dev
+call npm install --save trimble-connect-project-workspace-api
---
+++ node_modules/trimble-connect-project-workspace-api/license.md
@@ -0,0 +1,1 @@
+https://community.trimble.com/docs/DOC-10021
